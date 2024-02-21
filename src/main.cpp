e58284cf55fba9ee6d3f99b55276b5e78e287fe4
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2024-02-21 21:34:12 +0000

    Add multi file

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+
+int main(int argc, char const* argv[])
+{
+    test();
+    return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+
+#include <iostream>
+
+void test()
+{
+    std::cout << "Test Hello World" << std::endl;
+}
