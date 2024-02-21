8339b9725d64f75cc084b98213f39f29251e78ea
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2024-02-21 21:42:23 +0000

    Add OpenGL test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,58 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <cmath>
+
 #include "test.h"
 
-int main(int argc, char const* argv[])
+void changeSize(const int w, int h)
+{
+    if (h == 0)
+        h = 1;
+
+    const float ratio = static_cast<float>(w) * 1.0f / static_cast<float>(h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    gluLookAt(0.0f, 0.0f, 5.0f,
+              0.0f, 0.0f, -1.0f,
+              0.0f, 1.0f, 0.0f);
+
+    glutWireTeapot(1);
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
 {
     test();
-    return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("CG@DI");
+
+    glutReshapeFunc(changeSize);
+    glutIdleFunc(renderScene);
+    glutDisplayFunc(renderScene);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+
+    return 1;
 }
