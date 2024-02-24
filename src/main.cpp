5d8979445500d0f4bbf2cebb1a81141d9ae2801b
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2024-02-24 20:02:25 +0000

    feat: Add imgui and tinyxml2 test

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 #include <GL/glut.h>
 #endif
 
-#include <cmath>
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl2.h"
+#include "tinyxml2.h"
 
-#include "test.h"
+#include <cmath>
 
 void changeSize(const int w, int h)
 {
@@ -23,6 +26,30 @@ void changeSize(const int w, int h)
 
 void renderScene()
 {
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+    ImGui::NewFrame();
+    const ImGuiIO& io = ImGui::GetIO();
+
+    static float f = 0.0f;
+    static int counter = 0;
+
+    ImGui::Begin("Hello, world!");
+
+    ImGui::Text("This is some useful text.");
+
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
+    if (ImGui::Button("Button"))
+        counter++;
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", counter);
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+    ImGui::End();
+
+    ImGui::Render();
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glLoadIdentity();
@@ -32,12 +59,15 @@ void renderScene()
 
     glutWireTeapot(1);
 
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
     glutSwapBuffers();
 }
 
 int main(int argc, char** argv)
 {
-    test();
+    tinyxml2::XMLPrinter printer;
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100, 100);
@@ -48,11 +78,26 @@ int main(int argc, char** argv)
     glutIdleFunc(renderScene);
     glutDisplayFunc(renderScene);
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    ImGui::StyleColorsDark();
+    ImGui_ImplGLUT_Init();
+    ImGui_ImplOpenGL2_Init();
+
+    ImGui_ImplGLUT_InstallFuncs();
+
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     glutMainLoop();
+
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGLUT_Shutdown();
+    ImGui::DestroyContext();
 
     return 1;
 }
